f758b2fbd22ec02eac67ffdf4a10b534e387e4c5
mikeaussie <michael.pyatin@me.com>
2016-03-10 20:29:37 +1000

    added IBOutlets for the enemy, textLbl

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBOutlet weak var textLbl: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
